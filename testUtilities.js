109492646057897730a62927a4dee7a9118e7919
asteriasandippal <sandip.pal@asteria.co.in>
2018-03-30 16:49:44 +0530

    Added mocha Package and Used

---
+++ testUtilities.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+
+expect(true).to.be.true;
+function titleCase (title) {
+    return title[0].toUpperCase() + title.substring(1);
+}
+expect(titleCase('the great mouse detective')).to.be.a('string');
+
+expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
+
+expect('a').to.equal('A');
